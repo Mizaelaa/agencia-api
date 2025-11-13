3b6b8d65807973ecf663e17c1cfd55c7213fba2f
mizaelaa <mizaelanunes@gmail.com>
2025-11-13 19:28:16 -0300

    Removendo pastas

--- OneDrive/Documentos/agencia/agencia-api/src/main/java/com/nunes/agencia/agencia_api/repository/DestinoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nunes.agencia.agencia_api.repository;
-
-import com.nunes.agencia.agencia_api.model.Destino;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinoRepository extends JpaRepository<Destino, Long> {
-}
